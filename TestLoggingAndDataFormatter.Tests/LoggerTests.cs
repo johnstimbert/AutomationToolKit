503157c4872bd56becba04c66b5b646a8fbc5de0
john stimbert <johnstimbert@gmail.com>
2018-11-26 12:34:16 -0700

    Unit test project for logger and data formatter initial

---
+++ TestLoggingAndDataFormatter.Tests/LoggerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLoggingAndDataFormatter.Tests
+{
+    [TestClass]
+    public class LoggerTests
+    {
+        private const string _logPath = @"c:\logger";
+        private const string _logFileName = "logger.txt";
+
+        private Logger _logger;
+
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            _logger = new Logger(_logFileName, _logFileName);
+        }
+
+        [TestCleanup]
+        public void AfterEachTest()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
