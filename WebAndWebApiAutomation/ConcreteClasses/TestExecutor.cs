8e65d9d7be310d795944a5a5f1129240aa7c118d
Stimbert, John <John.Stimbert@bannerhealth.com>
2019-11-15 13:49:06 -0700

    #12 Code complete, no unit tests

--- WebAndWebApiAutomation/ConcreteClasses/TestExecutor.cs
+++ WebAndWebApiAutomation/ConcreteClasses/TestExecutor.cs
@@ -13,28 +13,27 @@ namespace WebAndWebApiAutomation
     internal sealed class TestExecutor : ITestExecutor
     {
         private LoggerWebAutoInternal _logger;
-        private TestDataCollector _testDataCollector;
+        private readonly TestDataCollector _testDataCollector;
 
         private string _testId;
         private string _resultsPath;
         private Guid _testRunId;
-
+        
 
         private TestExecutor() { }
         
         internal TestExecutor(LoggerWebAutoInternal logger = null, bool collectTestData = false, string resultsPath = null)
         {
             _logger = logger;
-            //TODO Uncomment when test data collector is ready
-            //if (collectTestData)
-            //{
-            //    _testDataCollector = new TestDataCollector();
+            if (collectTestData)
+            {
+                _testDataCollector = new TestDataCollector();
 
-            //    if (_testRunId == null)
-            //        _testRunId = Guid.NewGuid();
-            //}
+                if (_testRunId == null)
+                    _testRunId = Guid.NewGuid();
+            }
 
-            if(resultsPath != null)
+            if (resultsPath != null)
             {
                 _resultsPath = Path.Combine(resultsPath, DateTime.Now.ToString("MM.dd.yyyy_h:mm"));
             }
@@ -42,14 +41,14 @@ namespace WebAndWebApiAutomation
         }
 
         #region Events and Event Delegates
-        public static event TestBegunEvent TestBegun;
-        public delegate void TestBegunEvent(Guid runId, string testMethodName);
+        internal static event TestBegunEvent TestBegun;
+        internal delegate void TestBegunEvent(Guid runId);
 
-        public static event TestCompleteEvent TestComplete;
-        public delegate void TestCompleteEvent(Guid runId, string testMethodName, bool isSuccess, Exception error = null);
+        internal static event TestCompleteEvent TestComplete;
+        internal delegate void TestCompleteEvent(Guid runId, string testMethodName, bool isSuccess, Exception error = null);
 
         #endregion
-        
+
         /// <summary>
         /// Executes the test provided in the Action delegate.
         /// </summary>
@@ -65,7 +64,7 @@ namespace WebAndWebApiAutomation
             {
 
                 //Raise the TestBegun event to signal the TestDataCollector a new test is being executed
-                TestBegun?.Invoke(_testRunId, _testId);
+                TestBegun?.Invoke(_testRunId);
 
                 //If the loger is null execute without logging
                 if (_logger == null)
@@ -102,23 +101,35 @@ namespace WebAndWebApiAutomation
             }
             catch(Exception ex)
             {
-                var wae = new WebAutomationException(ex.ToString());
+                var tee = new TestExecutorException(ex.ToString());
 
                 if (_testDataCollector != null)
                 {
                     TakeScreenShot(webDriverManager, _testId, _resultsPath);
                     //Raise the TestComplete event to signal the TestDataCollector a test is completed. In this case it failed and we need to pass the exception.
-                    TestComplete?.Invoke(_testRunId, _testId, false, ex);
+                    TestComplete?.Invoke(_testRunId, _testId, false, tee);
                 }
 
                 if (_logger != null)
                 {
-                    _logger.Log(LogMessageType.TESTFAILED, wae.Message);
+                    _logger.Log(LogMessageType.TESTFAILED, tee.Message);
                     _logger.Log(LogMessageType.TESTINFO, $"[END]   =============== [FAILED] ===============");
                 }
 
-                throw wae;
+                throw tee;
             }
+        }
+
+        /// <summary>
+        /// Returns formatted test results that can be written to a log file or elsewhere
+        /// </summary>
+        /// <returns>Formatted test results</returns>
+        public string GetTestRunSummary()
+        {
+            if (_testDataCollector == null)
+                return "Please enabale test data collection when instantiating the TestExecutor";
+
+            return string.Join(Environment.NewLine, _testDataCollector.GetRunSummary());
         }
 
         #region Private Methods
--- WebAndWebApiAutomation/TestDataCollection/TestDataCollector.cs
+++ WebAndWebApiAutomation/TestDataCollection/TestDataCollector.cs
@@ -3,10 +3,119 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebAndWebApiAutomation.Exceptions;
 
 namespace WebAndWebApiAutomation.TestDataCollection
 {
     internal sealed class TestDataCollector
     {
+        private const string _nullTestFailedExceptionMessage = "Exception cannot be null and Exception.Message cannot be empty or whitespace when logging failed test data";
+        private const string _testRunChangedMessage = "Cannot change test run id in the middle of a test run";
+        private Guid _testRunId;
+
+        private int _totalTestCount = 0;
+        private int _totalTestsFailedCount = 0;
+        private int _totalTestsPassedCount = 0;
+
+        private List<string> _failedTestsListing;
+        private List<string> _passedTestsListing;
+
+        internal TestDataCollector()
+        {
+            TestExecutor.TestBegun += TestBegun;
+            TestExecutor.TestComplete += TestComplete;
+
+            _failedTestsListing = new List<string>();
+            _passedTestsListing = new List<string>();
+        }
+
+        internal List<string> GetRunSummary()
+        {
+            var runSummary = new List<string>();
+
+            runSummary.Add($"Summary For Test Run Id : {_testRunId}");
+            runSummary.Add($"Execution Date: {DateTime.Today}");
+            runSummary.Add("");
+            runSummary.Add("");
+            runSummary.Add($"{_totalTestCount} tests were run");
+            runSummary.Add($"{_totalTestsPassedCount} tests passed");
+            runSummary.Add($"{_totalTestsFailedCount} tests failed");
+            runSummary.Add("");
+            runSummary.Add("");
+            runSummary.Add($"Passed test methods");
+            foreach(string passed in _passedTestsListing)
+            {
+                runSummary.Add(passed);
+            }
+            runSummary.Add("");
+            runSummary.Add("");
+            runSummary.Add($"Failed test methods");
+            foreach (string failed in _failedTestsListing)
+            {
+                runSummary.Add(failed);
+            }
+
+            return runSummary;
+        }
+
+        private void TestComplete(Guid runId, string testMethodName, bool isSuccess, Exception error = null)
+        {
+            try
+            {
+                //Verify test run id matches the id of the current run. Throw exception if it does not.
+                if (_testRunId.Equals(runId))
+                {
+                    //We should not generate failure data if no message is passed
+                    if (!isSuccess &&
+                        (error == null || string.IsNullOrEmpty(error.Message) || string.IsNullOrWhiteSpace(error.Message)))
+                    {
+                        throw new TestDataCollectorException(_nullTestFailedExceptionMessage);
+                    }
+                    else if (!isSuccess)
+                    {
+                        _totalTestsFailedCount++;
+                        _failedTestsListing.Add($"Test Method {testMethodName} Failed with the following Message {error}");
+                    }
+                    else if (isSuccess)
+                    {
+                        _totalTestsPassedCount++;
+                        _passedTestsListing.Add(testMethodName);
+                    }
+                }
+                else
+                {
+                    throw new TestExecutorException(_testRunChangedMessage);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new TestDataCollectorException("Test Complete Call Failed", ex);
+            }
+        }
+
+        private void TestBegun(Guid runId)
+        {
+            try
+            {
+                //Assign the run id if this is the first test being executed
+                if (_testRunId == null)
+                    _testRunId = runId;
+
+                //Verify test run id matches the id of the current run. Throw exception if it does not.
+                if (_testRunId.Equals(runId))
+                {
+                    //Increment the total test count every time this event is raised
+                    _totalTestCount++;
+                }
+                else
+                {
+                    throw new TestExecutorException(_testRunChangedMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new TestDataCollectorException("Test Begun Call Failed", ex);
+            }
+        }
     }
 }
